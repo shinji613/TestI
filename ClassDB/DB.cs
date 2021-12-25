2fc6563223b7fd602ae24ecb64e881aa42d120ff
祺翔 黃 <yiyiseal3@yahoo.com.tw>
2021-12-25 10:54:54 +0800

    DataBase

--- ClassDB/DB.cs
+++ ClassDB/DB.cs
@@ -5,7 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassDB {
-    public class DB {
+    /// <summary>員工</summary>
+    public partial class Employees{
 
+        public static Employees Get(int employeeID) {
+            northwindDataContext nw = new northwindDataContext();
+
+            return nw.Employees.FirstOrDefault(a => a.EmployeeID == employeeID);
+        }
+    }
+
+    /// <summary>訂單</summary>
+    public partial class Orders {
+
+        public static Orders Get(int orderID) {
+            northwindDataContext nw = new northwindDataContext();
+
+            return nw.Orders.FirstOrDefault(a => a.OrderID == orderID);
+        }
+    }
+
+    /// <summary>顧客</summary>
+    public partial class Customers {
+
+        public static Customers Get(string customerID) {
+            northwindDataContext nw = new northwindDataContext();
+
+            return nw.Customers.FirstOrDefault(a => a.CustomerID == customerID);
+        }
     }
 }
