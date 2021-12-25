b1932bc9fe269e751a6d4611209c8bb76b0d3861
祺翔 黃 <yiyiseal3@yahoo.com.tw>
2021-12-25 11:15:49 +0800

    Employees

--- ClassDB/DB.cs
+++ ClassDB/DB.cs
@@ -5,13 +5,64 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassDB {
+    public class constant {
+        public static int 配號初始值 = 1;
+    }
+
     /// <summary>員工</summary>
-    public partial class Employees{
+    public partial class Employees {
 
         public static Employees Get(int employeeID) {
             northwindDataContext nw = new northwindDataContext();
 
-            return nw.Employees.FirstOrDefault(a => a.EmployeeID == employeeID);
+            return nw.Employees.FirstOrDefault(e => e.EmployeeID == employeeID);
+        }
+
+        private static int GetMaxSerial() {
+            northwindDataContext nw = new northwindDataContext();
+
+            return nw.Employees.ToArray().Length != 0 ?
+                  nw.Employees.Max(e => e.EmployeeID) + 1 : constant.配號初始值;
+        }
+
+        public static void New() {
+            northwindDataContext nw = new northwindDataContext();
+
+            Employees emp = new Employees();
+
+            int serial = GetMaxSerial();
+
+            emp.EmployeeID = serial;
+            emp.LastName = "LastName" + serial;
+            emp.FirstName = "FirstName" + serial;
+            emp.Address = "Address" + serial;
+
+            nw.Employees.InsertOnSubmit(emp);
+            nw.SubmitChanges();
+        }
+
+        public static void Update(int employeeID, string address) {
+            northwindDataContext nw = new northwindDataContext();
+
+            Employees emp = nw.Employees.FirstOrDefault(e => e.EmployeeID == employeeID);
+
+            if (emp == null) return;
+
+            emp.Address = address;
+
+            nw.SubmitChanges();
+        }
+
+        public static void Delete(int employeeID) {
+            northwindDataContext nw = new northwindDataContext();
+
+            Employees emp = nw.Employees.FirstOrDefault(e => e.EmployeeID == employeeID);
+
+            if (emp == null) return;
+
+            nw.Employees.DeleteOnSubmit(emp);
+
+            nw.SubmitChanges();
         }
     }
 
@@ -23,6 +74,8 @@ namespace ClassDB {
 
             return nw.Orders.FirstOrDefault(a => a.OrderID == orderID);
         }
+
+
     }
 
     /// <summary>顧客</summary>
@@ -35,3 +88,4 @@ namespace ClassDB {
         }
     }
 }
+
